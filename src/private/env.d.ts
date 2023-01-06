20a8dd90a6110bfd6ba01e9310505e4c4c919ddb
toolifelesstocode <toolifelesstocode@gmail.com>
2023-01-06 21:44:43 +0200

    refactor(.env): add typings file

---
+++ src/private/env.d.ts
@@ -0,0 +1,12 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            TOKEN: string;
+            GUILD_ID: string;
+            DEFAULT_H_ROLE_ID: string;
+            LOG_CHANNEL_ID: string;
+        }
+    }
+}
+
+export {}
