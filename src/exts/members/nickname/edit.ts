182f60d073955596b0c62b0225a04825e9a85c4d
toolifelesstocode <toolifelesstocode@gmail.com>
2023-01-07 14:03:05 +0200

    refactor: split actions into different files

---
+++ src/exts/members/nickname/edit.ts
@@ -0,0 +1,10 @@
+import { EventListener } from 'yuuko';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default new EventListener('guildMemberUpdate', async (guild, member, _oldMember, _ctx) => {
+    if (guild.id !== process.env.GUILD_ID) return;
+
+    if (member.nick !== 'o') {
+        return await member.edit({ nick: 'o' }, 'not o');
+    }
+});
