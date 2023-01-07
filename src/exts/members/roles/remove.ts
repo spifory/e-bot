858f9509c70affc849ecf5bb378cc6066548cf8e
Sham <thesham@protonmail.com>
2023-01-07 14:03:05 +0200

    refactor: split actions into different files

---
+++ src/exts/members/roles/remove.ts
@@ -0,0 +1,21 @@
+import { TextChannel } from 'eris';
+import { EventListener } from 'yuuko';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default new EventListener('guildMemberUpdate', async (guild, member, _oldMember, _ctx) => {
+    if (guild.id !== process.env.GUILD_ID) return;
+
+    if (!member.roles.includes(process.env.DEFAULT_O_ROLE_ID)) {
+        try {
+            return await member.addRole(process.env.DEFAULT_O_ROLE_ID, 'o');
+        } catch (error) {
+            const logChannel = guild.channels.get(process.env.LOG_CHANNEL_ID);
+            if (logChannel instanceof TextChannel) {
+                await logChannel.createMessage(
+                    `Error adding role to ${member.user.username}#${member.user.discriminator}: `,
+                    error,
+                );
+            }
+        }
+    }
+});
