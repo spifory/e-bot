cc394c70cbbdb77dc0325f052012ac0e5436f5b7
toolifelesstocode <toolifelesstocode@gmail.com>
2023-01-07 14:03:15 +0200

    feat: add message automod

---
+++ src/exts/messages/automod/create.ts
@@ -0,0 +1,20 @@
+import { GuildChannel, TextChannel, VoiceChannel } from 'eris';
+import { EventListener } from 'yuuko';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default new EventListener('messageCreate', async (msg, _ctx) => {
+    let channel: GuildChannel | undefined;
+    if (msg.channel instanceof (TextChannel || VoiceChannel)) {
+        channel = msg.channel;
+    }
+
+    if (
+        (msg.content !== channel.name) ||
+        (msg.embeds.length !== 0) ||
+        (msg.attachments.length !== 0) ||
+        (msg.stickerItems !== undefined) || // does not return a list for whatever reason
+        (msg.components.length !== 0)
+    ) {
+        return await msg.delete('not o');
+    }
+});
---
+++ src/exts/messages/automod/edits.ts
@@ -0,0 +1,14 @@
+import { GuildChannel, TextChannel, VoiceChannel } from 'eris';
+import { EventListener } from 'yuuko';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default new EventListener('messageUpdate', async (msg, oldMsg, _ctx) => {
+    let channel: GuildChannel | undefined;
+    if (msg.channel instanceof (TextChannel || VoiceChannel)) {
+        channel = msg.channel;
+    }
+
+    if (msg.content !== channel.name) {
+        return await msg.delete('not o');
+    }
+});
